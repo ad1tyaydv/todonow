cd68fdb2f1687b7dc18996752dee5ab5b7308ce0
Aditya <adityaydv12890@gmail.com>
2025-10-01 11:35:18 +0530

    UPDATED

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.BACKEND;
 
 function App() {
 
